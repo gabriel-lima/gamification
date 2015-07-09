c68e65f731c495a76b619e100104bf47f493bcbc
Gabriel <dev.gabriel.lima@gmail.com>
2015-07-08 21:21:42 -0300

    Adicionado comportamento dos botões

--- website/server.py
+++ website/server.py
@@ -1,5 +1,5 @@
 # encoding: utf-8
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 
 
 app = Flask(__name__)
@@ -7,12 +7,17 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello World'
+    return render_template('base.html')
 
 
 @app.route('/clientes/cadastrar', methods=['GET'])
 def novo_cliente():
     return render_template('clientes/novo.html')
+
+
+@app.route('/clientes/cadastrar', methods=['POST'])
+def criar_cliente():
+    return redirect(url_for('lista_de_clientes'))
 
 
 @app.route('/clientes', methods=['GET'])
@@ -24,4 +29,3 @@ if __name__ == '__main__':
     app.secret_key = '\xec\x84j\xcdY\xc7\xf7".\x0e\xf4o>\x0f\xc9\x88f\xf4svPG\xec\x8b'
     app.debug = True
     app.run()
-    
