528e69cc3276ad1e4082e186dba40bab07c05172
Gabriel <dev.gabriel.lima@gmail.com>
2015-07-11 13:36:02 -0300

    Renomeado arquivo de inicialização servidor e alterado portas de inicialização

--- webapplication/runserver.py
+++ webapplication/runserver.py
@@ -23,5 +23,4 @@ def novo_cenario():
 if __name__ == '__main__':
     app.secret_key = ')\xe8\xd3\x1e\xf3\x06$\x9ct\xb4\xcc$3\xd6\xf6/St\xd9\xad(\xe4h1'
     app.debug = True
-    app.run()
-    
+    app.run(port=5000)
--- website/runserver.py
+++ website/runserver.py
@@ -39,4 +39,4 @@ def lista_de_clientes():
 if __name__ == '__main__':
     app.secret_key = '\xec\x84j\xcdY\xc7\xf7".\x0e\xf4o>\x0f\xc9\x88f\xf4svPG\xec\x8b'
     app.debug = True
-    app.run()
+    app.run(port=5001)
