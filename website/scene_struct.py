c2005d360ed674864190bad2cf836d5f277542f4
Gabriel <dev.gabriel.lima@gmail.com>
2015-10-25 21:50:26 -0200

    Classe de DTO de cenário

---
+++ website/scene_struct.py
@@ -0,0 +1,5 @@
+class SceneStruct(object):
+    def __init__(self, scene_id=None, format_xml=None, script_python=None):
+        self.scene_id = scene_id
+        self.format_xml = format_xml
+        self.script_python = script_python
