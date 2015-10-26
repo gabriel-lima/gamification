2e000e2abf615a4e131c4556bcf00dfeffcd1eb4
Gabriel <dev.gabriel.lima@gmail.com>
2015-10-25 23:13:59 -0200

    Adicionado eventos de cupom de desconto na api

--- website/api.py
+++ website/api.py
@@ -14,19 +14,33 @@ class GamifyCommand(object):
     def command(self, name):
         if name == 'frete_gratis':
             self.data['script'] = '''
-                humane.log('Legal! Adicione mais um produto e você ganhará o frete grátis.');
+                humane.log('Legal! Adicione mais um produto e você ganhará o frete grátis.', function() { callback(); });
             '''
+        if name == 'cupom_desconto':
+            self.data['script'] = '''
+                humane.log('Parabéns! Você acaba de ganhar um cupom de desconto para a sua próxima compra.', function() { callback(); });
+            '''
+
+
+class Engine(object):
+    def __init__(self, script):
+        self.script = script
+
+    def execute(self, gamify, event):
+        if self.script:
+            exec self.script
 
 
 @api.route('/api/track', methods=['GET'])
 def track():
-    gamify = GamifyCommand()
-    scenes = scene_gateway.all()
+    event = request.args.get('event', None)
 
+    gamify = GamifyCommand()
+
+    scenes = scene_gateway.all()
     for scene in scenes:
-        if scene.script_python:
-            event = request.args.get('event', None)
-            exec scene.script_python
+        engine = Engine(scene.script_python)
+        engine.execute(gamify, event)
 
     jsonp = '%s(%s)' % (request.args.get('callback'), json.dumps(gamify.data))
     return Response(jsonp, content_type='text/javascript')
